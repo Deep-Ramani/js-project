052a5a2ad47161d24ad8cad087c57bf607c9da41
deep_ramani <41836849+deepramani8505@users.noreply.github.com>
2021-02-19 19:00:51 +0530

    indexfile

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Caffeine Rush | The Happy Hours</title>
+	
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<!-- <script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script> -->
+	<!--// Meta tag Keywords -->
+	
+	<!-- css files -->
+	<link rel="shortcut icon" href="ico/logo1.ico">
+	<link rel="stylesheet" href="css/mp.css">
+	<link rel="stylesheet" href="css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+	<link rel="stylesheet" href="css/style1.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+	<link rel="stylesheet" href="css/fontawesome-all.css"> <!-- Font-Awesome-Icons-CSS -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/video.css">
+	<link rel="stylesheet" type="text/css" href="css/popuo-box.css">
+
+	<!-- //css files -->
+	<script src="js/jquery.min.js"></script>
+  	<script src="js/bootstrap.min.js"></script>
+	
+	<link href="css/prettyPhoto.css" rel="stylesheet" type="text/css" />
+	
+	<!-- fonts -->
+		<!-- <link href="https://fonts.googleapis.com/css?family=Kirang+Haerang" rel="stylesheet"> -->
+		<!-- <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet"> -->
+		<!-- <link href="https://fonts.googleapis.com/css?family=Caveat" rel="stylesheet"> -->
+	<!-- fonts -->
+</head>
+<body>
+
+<!-- Navigation -->
+<?php
+	include("header.php");
+?>
+<!-- //Navigation -->
