3c3bd42cea1d817f1ca7ac1d368df1d01d5c4a77
deep_ramani <41836849+deepramani8505@users.noreply.github.com>
2021-02-20 22:17:47 +0530

    JS File

--- caffeine_rush/index.php
+++ caffeine_rush/index.php
@@ -96,3 +96,261 @@
 	<div class="clearfix"></div>
 </div>
 <!--//Slider-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- js-scripts -->	
+
+
+
+<script src="js/jquery-2.2.3.min.js"></script>
+
+<!-- Banner Responsive slider -->
+<script src="js/responsiveslides.min.js"></script>
+	<!-- // Banner Responsiveslides -->
+	<!--pop-up-box -->
+	<script src="js/jquery.magnific-popup.js"></script>
+	<script>
+		$(document).ready(function () {
+			// $(document).on("click", "#small-dialog1",function(){
+			// 	// var src= $("#add-video").data('src');
+			// 	$("#add-video").attr('src', 'videos/video.mp4');
+			// });
+
+			$('.popup-with-zoom-anim').magnificPopup({
+				type: 'inline',
+				fixedContentPos: false,
+				fixedBgPos: true,
+				overflowY: 'auto',
+				closeBtnInside: true,
+				preloader: false,
+				midClick: true,
+				autoPlay: false,
+				removalDelay: 300,
+				mainClass: 'my-mfp-zoom-in',
+				callbacks: {
+        			open: function() {
+							$("#add-video").attr('src','videos/video.mp4');
+					},
+			        close: function() {
+			        	// debugger;
+			            // Reset video on close:
+			            setTimeout(function() {
+			            	$("#add-video").attr('src',' ');
+			            },100);
+
+			        }
+			    }
+			});
+		});
+	</script>
+	<!-- //pop-up-box -->
+
+
+	<script>
+		// You can also use "$(window).load(function() {"
+		$(function () {
+			// Slideshow 3
+			$("#slider3").responsiveSlides({
+				auto: true,
+				pager: false,
+				nav: true,
+				speed: 500,
+				namespace: "callbacks",
+				before: function () {
+					$('.events').append("<li>before event fired.</li>");
+				},
+				after: function () {
+					$('.events').append("<li>after event fired.</li>");
+				}
+			});
+
+		});
+	</script>
+	<!-- // Banner Responsive slider -->
+
+
+
+
+
+	<!-- stats -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/jquery.countup.js"></script>
+	<script>
+		$('.counter').countUp();
+	</script>
+	<!-- //stats -->
+	
+
+
+
+	<!-- search-bar -->
+	<script src="js/main.js"></script>
+	<!-- //search-bar -->
+	
+
+
+
+
+	<!-- start-smoth-scrolling -->
+	<script src="js/SmoothScroll.min.js"></script>
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<!-- <script type="text/javascript" src="js/easing.js"></script> -->
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+			});
+		});
+	</script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+
+
+	</script>
+	<!-- start-smoth-scrolling -->
+
+
+	<!-- jQuery-Photo-filter-lightbox-Gallery-plugin -->
+	<script type="text/javascript" src="js/jquery-1.7.2.js"></script>
+	<!-- <script src="js/jquery.quicksand.js" type="text/javascript"></script> -->
+	<script src="js/script.js" type="text/javascript"></script>
+	<script src="admin-js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="admin-js/scroll-scripts.js"></script>
+	<!-- <script src="js/jquery.prettyPhoto.js" type="text/javascript"></script> -->
+	<!-- //jQuery-Photo-filter-lightbox-Gallery-plugin -->
+
+
+
+
+
+
+
+
+
+
+	
+	
+
+	<!-- smooth-scrolling-of-move-up -->
+
+
+
+<!-- //js-scripts -->
+
+</body>
+</html>
