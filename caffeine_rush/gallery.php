ee97dcddc59bc85f572a1b22a840398ec4360ffd
deep_ramani <41836849+deepramani8505@users.noreply.github.com>
2021-02-21 23:22:54 +0530

    Images1

---
+++ caffeine_rush/gallery.php
@@ -0,0 +1,121 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Caffeine Rush | The Happy &#8377;</title>
+	
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta &#8377;et="utf-8">
+	
+	<!--// Meta tag Keywords -->
+	
+	<!-- css files -->
+	<link rel="shortcut icon" href="ico/logo1.ico">
+	<link rel="stylesheet" href="css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+	<link rel="stylesheet" href="css/fontawesome-all.css"> <!-- Font-Awesome-Icons-CSS -->
+	<link rel="stylesheet" type="text/css" href="css\bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style1.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+   <script src="js/jquery.min.js"></script>
+  	<script src="js/bootstrap.min.js"></script>
+
+    <!-- css for hover -->
+
+	<link rel="stylesheet" href="css/hover.css" />
+    <!-- css for hover -->
+
+
+	<!-- //css files -->
+
+	<link href="css/prettyPhoto.css" rel="stylesheet" type="text/css" />
+
+	<!-- fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Forum|Handlee|Kaushan+Script" rel="stylesheet">
+		<!-- <link href="https://fonts.googleapis.com/css?family=Kirang+Haerang" rel="stylesheet"> -->
+		<!-- <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet"> -->
+		<!-- <link href="https://fonts.googleapis.com/css?family=Caveat" rel="stylesheet"> -->
+	<!-- fonts -->
+
+	
+	
+</head>
+
+<body>
+
+<!-- Navigation -->
+<?php
+	include("header.php");
+?>
+<!-- //Navigation -->
+
+<!-- inner page banner -->
+<section class="inner_banner">
+	<div class="dot1">
+	</div>
+</section>
+<!-- inner page banner -->
+
+
+<!--Slider-->
+
+<!-- o -->
+<!--//Slider-->
+
+<section id="blog" class="section-padding wow fadeInUp delay-05s">
+    <div class="margin12">
+    	<h2 class="heading text-center text-uppercase mb-5"> New Items </h2>
+      	<div class="row">
+
+	        <div class="col-md-3 col-sm-4 col-xs-12">
+	          <div class="blog-sec">	
+	            <div class="blog-img">
+	                <img class="imgs" src="images1/Long Black.jpg" class="img-responsive">
+	            </div>
+	            <div class="blog-info">
+	              <h2>Long Black</h2>              
+	            </div>
+	          </div>
+	        </div>
+
+
+	        <div class="col-md-3 col-sm-4 col-xs-12">
+	          <div class="blog-sec">
+	            <div class="blog-img">
+	                <img class="imgs" src="images1/milkshakes/monster-candy.jpeg" class="img-responsive">
+	            </div>
+	            <div class="blog-info">
+	              <h2>Monster Candy</h2>              
+	            </div>
+	          </div>
+	        </div>
+
+
+	        <div class="col-md-3 col-sm-4 col-xs-12">
+	          <div class="blog-sec">
+	            <div class="blog-img">
+	                <img class="imgs" src="images1/cakes/blue.jpg" class="img-responsive">
+	            </div>
+	            <div class="blog-info">
+	              <h2>Blue Bucks Cake</h2>
+	            </div>
+	          </div>
+	        </div>
+
+
+			<div class="col-md-3 col-sm-4 col-xs-12">
+	          <div class="blog-sec">
+	            <div class="blog-img">
+	                <a href="google.com"><img class="imgs" src="images1/cakes/red.jpg" class="img-responsive" ></a>
+	            </div>
+	            <div class="blog-info">
+	              <h2>Black Cherry Cake</h2>
+	            </div>
+	          </div>
+	        </div>
+	        
+      	</div>
+    </div>
+</section>
