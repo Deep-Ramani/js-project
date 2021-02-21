de8e73ada101712a9e2961ec53c46acfb956f4c3
deep_ramani <41836849+deepramani8505@users.noreply.github.com>
2021-02-21 22:32:23 +0530

    Video & Footer

---
+++ caffeine_rush/footer.php
@@ -0,0 +1,22 @@
+<!-- footer -->	
+<footer>
+	<div class="footer12 py-3 py-md-4">
+		<div class="row">
+			<div class="col-lg-5 col-md-12 f_pdng">
+				<p class="py-lg-4">© 2018 Caffeine Rush. All Rights Reserved |<br> Developed & Design by <a href="https://www.instagram.com/parth_pokar_77" target="_blank">Parth Pokar</a> &  <a href=https://www.instagram.com/parth_pokar_77"" target="_blank">Sheetal Pandya</a> & <a href="#" target="_blank">Raj Nakum</a></p>
+			</div>
+			<div class="col-lg-4 col-md-12 logo text-center f_pdng">
+				<a href="homepage.php" class="brand-text">Caffeine Rush</a>
+			</div>
+			<div class="col-lg-3 col-md-12 f_pdng">
+				<ul class="py-lg-4">
+					<li class="mx-2"><a href="homepage.php">Home</a></li>
+					<li class="mx-2"><a href="gallery.php">Menu</a></li>
+					<li class="mx-2"><a href="services.php">Services</a></li>
+					<li class="mx-2"><a href="contact.php">Contact Us</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+<!-- footer -->
