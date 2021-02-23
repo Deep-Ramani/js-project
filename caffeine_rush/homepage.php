c95ae7735a8b41a0e640395800f4a69b39a6c142
deep_ramani <41836849+deepramani8505@users.noreply.github.com>
2021-02-23 10:40:09 +0530

    changes

---
+++ caffeine_rush/homepage.php
@@ -0,0 +1,540 @@
+
+
+
+<?php
+	session_start();
+	$_SESSION["currentPage"] = 1;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Caffeine Rush | The Happy Hours</title>
+	
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<!-- <script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script> -->
+	<!--// Meta tag Keywords -->
+	
+	<!-- css files -->
+	<link rel="shortcut icon" href="ico/logo1.ico">
+	<link rel="stylesheet" href="css/mp.css">
+	<link rel="stylesheet" href="css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+	<link rel="stylesheet" href="css/style1.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+	<link rel="stylesheet" href="css/fontawesome-all.css"> <!-- Font-Awesome-Icons-CSS -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/video.css">
+	<link rel="stylesheet" type="text/css" href="css/popuo-box.css">
+	<!-- //css files -->
+	<script src="js/jquery.min.js"></script>
+  	<script src="js/bootstrap.min.js"></script>
+	
+	<link href="css/prettyPhoto.css" rel="stylesheet" type="text/css" />
+	
+	<!-- fonts -->
+		<!-- <link href="https://fonts.googleapis.com/css?family=Kirang+Haerang" rel="stylesheet"> -->
+		<!-- <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet"> -->
+		<!-- <link href="https://fonts.googleapis.com/css?family=Caveat" rel="stylesheet"> -->
+	<!-- fonts -->
+</head>
+
+<body>
+
+<!-- Navigation -->
+<?php
+	include("header.php");
+?>
+<!-- //Navigation -->
+
+<!--Slider-->
+<div class="slider">
+	<div class="callbacks_container">
+		<ul class="rslides" id="slider3">
+			<li class="img-responsive">
+				<div class="slider-img1">
+					<div class="dot">
+						<div class="container">
+							<div class="slider_banner_info_w3ls text-center">
+							</div>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="img-responsive">
+				<div class="slider-img2 ">
+					<div class="dot">
+						<div class="container">
+							<div class="slider_banner_info_w3ls text-center">
+							</div>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="img-responsive">
+				<div class="slider-img3 ">
+					<div class="dot">
+						<div class="container">
+							<div class="slider_banner_info_w3ls text-center">
+							</div>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="img-responsive">
+				<div class="slider-img4 ">
+					<div class="dot">
+						<div class="container">
+							<div class="slider_banner_info_w3ls text-center">
+							</div>
+						</div>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+	<div class="clearfix"></div>
+</div>
+<!--//Slider-->
+
+<!-- About us -->
+<section class="about py-5">
+	<div>
+		<h3 class="heading text-center text-uppercase mb-5"> What We Have </h3>
+		
+		<section id="blog" class="section-padding wow fadeInUp delay-05s margins">
+		    <div>
+		      <div class="row">
+		        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+		          <div class="blog-sec">
+		            <div class="blog-img">
+		              <a href="gallery.php">
+		                <img src="images/about4.jpg" class="img-responsive height12">
+		              </a>
+		            </div>
+		            <div class="blog-info">
+		              <h2>Coffees</h2>
+		            </div>
+		          </div>
+		        </div>
+		        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+		          <div class="blog-sec">
+		            <div class="blog-img">
+		              <a href="gallery.php">
+		                <img src="images/about5.jpg" class="img-responsive height12">
+		              </a>
+		            </div>
+		            <div class="blog-info">
+		              <h2>Teas</h2>
+		            </div>
+		          </div>
+		        </div>
+		        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+		          <div class="blog-sec">
+		            <div class="blog-img">
+		              <a href="gallery.php">
+		                <img src="images/about6.jpeg" class="img-responsive height12">
+		              </a>
+		            </div>
+		            <div class="blog-info">
+		              <h2>Milk Shakes</h2>
+		            </div>
+		          </div>
+		        </div>
+		        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+		          <div class="blog-sec">
+		            <div class="blog-img">
+		              <a href="gallery.php">
+		                <img src="images/mousse.jpg" class="img-responsive height12">
+		              </a>
+		            </div>
+		            <div class="blog-info">
+		              <h2>Cakes</h2>
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		    </div>
+		  </section>
+				
+			</div>
+		</section>
+<!-- //About us -->
+
+
+<!-- /stats -->
+<section class="stats_test pb-5 container-fluid">
+	<div class="row inner_stat_wthree_agileits">
+		<div class="col-sm-3 col-6 py-5 stats_left counter_grid">
+			<i class="far fa-building"></i>
+			<p class="counter">3</p>
+			<h4>Branches</h4>
+		</div>
+		<div class="col-sm-3 col-6 py-5 stats_left counter_grid1">
+			<i class="fas fa-users"></i>
+			<p class="counter">12</p>
+			<h4>Qualified staff</h4>
+		</div>
+		<div class="col-sm-3 col-6 py-5 stats_left counter_grid2">
+			<i class="far fa-edit"></i>
+			<p class="counter">51</p>
+			<h4>Items</h4>
+		</div>
+		<div class="col-sm-3 col-6 py-5 stats_left counter_grid3">
+			<i class="far fa-smile"></i>
+			<p class="counter">1501</p>
+			<h4>Happy Customers</h4>
+		</div>
+	</div>
+</section>
+<!-- //stats -->
+
+<!-- video -->
+<section class="py5">
+	<div>
+		<h3 class="heading text-center text-uppercase mb-5"> make it yourself </h3>
+		<div class="video-grid1">
+			<div class="demo-bg1">
+				<div class="pop-bg text-center position-relative py-5">
+					<h2 class="w3ls-title  text-capitalize pt-lg-5 pt-sm-3 ">espresso yourself</h2>
+					<h4 class="w3l-sub pt-3 pb-5 text-white">stay grounded</h4>
+					<div class="arrow-container animated fadeInDown">
+						<a href="#small-dialog1" class="arrow-2 popup-with-zoom-anim">
+							<i class="fa fa-play" aria-hidden="true"></i>
+						</a>
+						<div class="arrow-1 animated hinge infinite zoomIn"></div>
+					</div>
+					<!--  video-button-popup -->
+					<div id="small-dialog1" class="mfp-hide">
+						<div class="agileits_modal_body">
+							<iframe src=" " id="add-video"></iframe>
+						</div>
+					</div>
+					<!-- // video-button-popup -->
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //video -->
+
+<!--Team-->
+<section class="Team py-5">
+	<div class="container">
+	<h3 class="heading text-center text-uppercase mb-5"> Our staff </h3>
+		<div class="row inner-sec-w3layouts-agileinfo">
+			<div class="col-md-4 col-sm-12 team-grids">
+				<img src="images/p12.jpg" class="img-responsive" alt="">
+				<div class="team-info">
+					<div class="caption">
+						<h4>Parth Pokar</h4>
+						<h6>Barista &</h6>
+						<h6>Pastry chef</h6>
+					</div>
+					<div class="social-icons-section">
+						<a class="fac" href="#">
+							<i class="fab fa-facebook"></i>
+						</a>
+						<a class="twitter" href="#">
+							<i class="fab fa-twitter-square"></i>
+						</a>
+						<a class="pinterest" href="#">
+							<i class="fab fa-instagram"></i>
+						</a>
+
+					</div>
+
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-12 team-grids">
+				<img src="images/p112.jpg" class="img-responsive" alt="">
+				<div class="team-info">
+					<div class="caption">
+						<h4>Sheetal Pandya</h4>
+						<h6>Tea Sommelier</h6>
+					</div>
+					<div class="social-icons-section">
+						<a class="fac" href="#">
+							<i class="fab fa-facebook"></i>
+						</a>
+						<a class="twitter" href="#">
+							<i class="fab fa-twitter-square"></i>
+						</a>
+						<a class="pinterest" href="#">
+							<i class="fab fa-instagram"></i>
+						</a>
+
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-12 team-grids">
+				<img src="images/p13.jpg" class="img-responsive" alt="">
+				<div class="team-info">
+					<div class="caption">
+						<h4>Raj Nakum</h4>
+						<h6>Frappista</h6>
+					</div>
+					<div class="social-icons-section">
+						<a class="fac" href="#">
+							<i class="fab fa-facebook"></i>
+						</a>
+						<a class="twitter" href="#">
+							<i class="fab fa-twitter-square"></i>
+						</a>
+						<a class="instagram" href="#">
+							<i class="fab fa-instagram"></i>
+						</a>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--//Team-->
+
+
+
+<div class="bgimg-2 w3-display-container w3-opacity-min">
+  <div class="w3-display-middle">
+    <!-- <p><span class="w3-xxlarge w3-text-white w3-wide parallax">&nbsp;&nbsp;With the great</span><br>
+  	<span class="w3-xxxlarge w3-text-white w3-wide"><strong>~ HISTORY ~</strong></span></p> -->
+  	<img src="images1/history1.png" class="history-img w3-wide parallax">
+  </div>
+</div>
+
+
+
+
+<!-- Newsletter -->
+<section class="newsletter text-center py-5">
+	<div class="container">
+		<h3 class="heading text-center text-uppercase mb-5"> Give review </h3>
+		
+		<div class="subscribe_inner">
+			<p class="mb-4">Thanks to visit <strong>Caffiene Rush</strong>. You will find it easy way to order the products on your own choice. If you like our service just put your best review! </p>
+			<form action="#" method="post">
+				<input type="textarea" placeholder="Type in your way" required="">
+				<input type="submit" class="mt-3" value=" Send ">
+			</form>
+			<!-- <div class="social mt-4">
+				<h4>Follow us</h4>
+				<ul class="d-flex mt-3 justify-content-center">
+					<li class="mx-2"><a href="#"><span class="fab fa-facebook"></span></a></li>
+					<li class="mx-2"><a href="#"><span class="fab fa-instagram"></span></a></li>
+					<li class="mx-2"><a href="#"><span class="fab fa-google"></span></a></li>
+				</ul>
+			</div> -->
+		</div>
+		
+	</div>
+</section>
+<!-- //Newsletter -->
+
+<?php
+	include("footer.php");
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- js-scripts -->		
+
+
+
+	<!-- js -->
+	<!-- <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script> -->
+	<script src="js/jquery-2.2.3.min.js"></script>
+	<!-- <script type="text/javascript" src="js/bootstrap.js"></script>  -->
+	<!-- Necessary-JavaScript-File-For-Bootstrap --> 
+	<!-- //js -->	
+	
+
+
+
+
+	<!-- Banner Responsive slider -->
+	<script src="js/responsiveslides.min.js"></script>
+	<!-- // Banner Responsiveslides -->
+	
+
+	
+	<!--pop-up-box -->
+	<script src="js/jquery.magnific-popup.js"></script>
+	<script>
+		$(document).ready(function () {
+			// $(document).on("click", "#small-dialog1",function(){
+			// 	// var src= $("#add-video").data('src');
+			// 	$("#add-video").attr('src', 'videos/video.mp4');
+			// });
+
+			$('.popup-with-zoom-anim').magnificPopup({
+				type: 'inline',
+				fixedContentPos: false,
+				fixedBgPos: true,
+				overflowY: 'auto',
+				closeBtnInside: true,
+				preloader: false,
+				midClick: true,
+				autoPlay: false,
+				removalDelay: 300,
+				mainClass: 'my-mfp-zoom-in',
+				callbacks: {
+        			open: function() {
+							$("#add-video").attr('src','videos/video.mp4');
+					},
+			        close: function() {
+			        	// debugger;
+			            // Reset video on close:
+			            setTimeout(function() {
+			            	$("#add-video").attr('src',' ');
+			            },100);
+
+			        }
+			    }
+			});
+		});
+	</script>
+	<!-- //pop-up-box -->
+
+
+	<script>
+		// You can also use "$(window).load(function() {"
+		$(function () {
+			// Slideshow 3
+			$("#slider3").responsiveSlides({
+				auto: true,
+				pager: false,
+				nav: true,
+				speed: 500,
+				namespace: "callbacks",
+				before: function () {
+					$('.events').append("<li>before event fired.</li>");
+				},
+				after: function () {
+					$('.events').append("<li>after event fired.</li>");
+				}
+			});
+
+		});
+	</script>
+	<!-- // Banner Responsive slider -->
+
+
+
+
+
+	<!-- stats -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/jquery.countup.js"></script>
+	<script>
+		$('.counter').countUp();
+	</script>
+	<!-- //stats -->
+	
+
+
+
+	<!-- search-bar -->
+	<script src="js/main.js"></script>
+	<!-- //search-bar -->
+	
+
+
+
+
+	<!-- start-smoth-scrolling -->
+	<script src="js/SmoothScroll.min.js"></script>
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<!-- <script type="text/javascript" src="js/easing.js"></script> -->
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+			});
+		});
+	</script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+
+
+	</script>
+	<!-- start-smoth-scrolling -->
+
+
+	<!-- jQuery-Photo-filter-lightbox-Gallery-plugin -->
+	<script type="text/javascript" src="js/jquery-1.7.2.js"></script>
+	<!-- <script src="js/jquery.quicksand.js" type="text/javascript"></script> -->
+	<script src="js/script.js" type="text/javascript"></script>
+	<!-- <script src="js/jquery.prettyPhoto.js" type="text/javascript"></script> -->
+	<!-- //jQuery-Photo-filter-lightbox-Gallery-plugin -->
+
+
+
+
+
+
+
+
+
+
+	
+	
+
+	<!-- smooth-scrolling-of-move-up -->
+
+
+
+<!-- //js-scripts -->
+
+</body>
+</html>
