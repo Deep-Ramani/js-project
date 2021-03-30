04558aed9269d96f8f5eef47dab21f2669083998
deep_ramani <41836849+deepramani8505@users.noreply.github.com>
2021-03-30 10:42:09 +0530

    loginerror page

---
+++ caffeine_rush/loginerror.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>! Error</title>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,700" rel="stylesheet">
+
+	<!-- Custom stlylesheet -->
+	<link type="text/css" rel="stylesheet" href="css/errorpage.css" />
+	<link rel="shortcut icon" href="assets/ico/logo1.ico">
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+</head>
+
+<body>
+
+	<div id="notfound">
+		<div class="err-img"><img src="assets/img/backgrounds/404.png"></div>
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>Ouch!</h1>
+				<h2>You Have Not Login Yet!!</h2>
+			</div>
+			<a onClick="javascript:history.go(-1)">Go TO Previous Page</a>
+		</div>
+	</div>
+
+
+	<!-- <script src="assets/js/jquery-1.11.1.min.js"></script>
+	<script src="assets/js/jquery.backstretch.min.js"></script>
+	<script>
+	    jQuery(document).ready(function() {
+	    
+	    /*
+	        Fullscreen background
+	    */
+	    $.backstretch("assets/img/backgrounds/17.1.jpg");
+	   
+	    
+	    
+	});
+	</script> -->
+
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
