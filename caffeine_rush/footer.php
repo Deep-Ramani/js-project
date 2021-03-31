111e656c9c1b79beb03cef901996fd558d7dad38
deep_ramani <41836849+deepramani8505@users.noreply.github.com>
2021-03-31 14:46:00 +0530

    forgot password page

--- caffeine_rush/footer.php
+++ caffeine_rush/footer.php
@@ -3,14 +3,14 @@
 	<div class="footer12 py-3 py-md-4">
 		<div class="row">
 			<div class="col-lg-5 col-md-12 f_pdng">
-				<p class="py-lg-4">© 2018 Caffeine Rush. All Rights Reserved |<br> Developed & Design by <a href="#" target="_blank">Deep Ramani</a> &  <a href="#" target="_blank">Ajay Markana</a>
+				<p class="py-lg-4">© 2018 Caffeine Rush. All Rights Reserved |<br> Developed & Design by <a href="#" target="_blank">Deep Ramani</a> &  <a href="#" target="_blank">Ajay Markana</a> 
 			</div>
 			<div class="col-lg-4 col-md-12 logo text-center f_pdng">
 				<a href="homepage.php" class="brand-text">Caffeine Rush</a>
 			</div>
 			<div class="col-lg-3 col-md-12 f_pdng">
 				<ul class="py-lg-4">
-					<li class="mx-2"><a href="index.php">Home</a></li>
+					<li class="mx-2"><a href="homepage.php">Home</a></li>
 					<li class="mx-2"><a href="gallery.php">Menu</a></li>
 					<li class="mx-2"><a href="services.php">Services</a></li>
 					<li class="mx-2"><a href="contact.php">Contact Us</a></li>
